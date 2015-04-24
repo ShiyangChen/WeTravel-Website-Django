5cc33ab398a79ca8984e2b6beae5d23d620cef02
ShiyangChen <chshysy910811@gmail.com>
2015-04-24 16:52:33 -0500

    change templates

--- wetravel/admin.py
+++ wetravel/admin.py
@@ -3,9 +3,5 @@ from wetravel.models import *
 
 # Register your models here.
 
-<<<<<<< HEAD
 admin.site.register([UserProfile, Group])
-admin.site.register([Region, Place])
-=======
-admin.site.register([UserProfile, Group,Post,Region])
->>>>>>> 66d712b92ed9ecebe10ec5b39e1c0189c4c45a7f
+admin.site.register([Region, Place, Post])
--- wetravel/urls.py
+++ wetravel/urls.py
@@ -9,13 +9,10 @@ urlpatterns = patterns('',
     url(r'^signup/', views.signup, name='signup'),
     url(r'^login/', views.user_login, name='login'),
     url(r'^logout/', views.user_logout, name='logout'),
-<<<<<<< HEAD
     url(r'^friends/', views.friends, name='friends'),
     url(r'^send_friend_request/', views.send_friend_request, name='send_friend_request'),
     url(r'^requests/', views.requests, name='requests'),
     url(r'^addplace/', views.add_place, name='addplace'),
-    url(r'^profile/', views.show_profile, name='profile')
-=======
     url(r'^create-post/$', views.create_post),
     url(r'^createpost/$', views.createpost),
     url(r'^profile/$',views.profile),
@@ -23,7 +20,6 @@ urlpatterns = patterns('',
     url(r'^delete/(\d+)/$',views.delete),
     url(r'^privacy_choose/(\d+)/$',views.privacy_choose),
     url(r'^privacy/(\d+)/$',views.privacy),
->>>>>>> 66d712b92ed9ecebe10ec5b39e1c0189c4c45a7f
 )
 
 urlpatterns += staticfiles_urlpatterns()
